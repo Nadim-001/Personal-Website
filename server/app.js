1c6ec6833563ecb7ef17a5dc584dcc2c05f351f2
Nadim-001 <mdnadimahmed.001@gmail.com>
2023-11-02 20:08:51 +0000

    Added Authenticator middleware as well as Token and User models, controllers, routes and setup databases to store users and tokens. AND tests for them.

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const exampleRouter = require("./routers/examples");
+const exampleRouter = require('./routers/examples');
+const userRouter = require('./routers/users');
 
 const app = express();
 
@@ -9,11 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("This is an example API");
+app.get('/', (req, res) => {
+  res.send('This is an example API');
 });
 
 // Routes
-app.use("/examples", exampleRouter);
+app.use('/examples', exampleRouter);
+app.use('/users', userRouter);
 
 module.exports = app;
