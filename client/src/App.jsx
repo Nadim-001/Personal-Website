e8a2750ca861205e16e682bf9c9558175a54b914
Nadim-001 <mdnadimahmed.001@gmail.com>
2024-03-04 18:12:51 +0000

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Header, Protected } from './components';
 import './App.css';
-import {
-  HomePage,
-  AboutPage,
-  NotFoundPage,
-  LoginPage,
-  RegisterPage,
-} from './pages';
+import { HomePage, AboutPage, NotFoundPage } from './pages';
 import { ExampleProvider } from './contexts';
 //Don't forget to change name of Provider
 
@@ -20,16 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<HomePage />} />
-          <Route
-            path="/about"
-            element={
-              <Protected>
-                <AboutPage />
-              </Protected>
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/*" element={<NotFoundPage />} />
         </Route>
       </Routes>
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,3 @@
 export { default as HomePage } from './Home';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as AboutPage } from './AboutPage';
-export { default as LoginPage } from './LoginPage';
-export { default as RegisterPage } from './RegisterPage';
